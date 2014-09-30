9fc70c2dd2e4e2d46df8ac26041749ed49a528e4
Gerardo Rosas <gerardo.dr@gmail.com>
2014-09-29 20:08:08 -0500

    Draw list of points

--- grails-app/services/mx/nissan/service/DrawPathService.groovy
+++ grails-app/services/mx/nissan/service/DrawPathService.groovy
@@ -16,18 +16,14 @@ class DrawPathService{
   def final Integer IMG_HEIGTH = 500
 
   def drawTestPath(def imgPath){
-    println "Dibuja rutaaaa"
 
     def mazeImage = new BufferedImage(IMG_WIDTH, IMG_HEIGTH , BufferedImage.TYPE_INT_RGB)
-    def g2 = mazeImage.createGraphics()
-
-    def img1 = ImageIO.read(new File(imgPath))
-    g2.setBackground(Color.WHITE)
-    g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGTH)
-    g2.drawImage(img1, 0, 0, IMG_WIDTH, IMG_HEIGTH, null)
-    g2.setColor(Color.RED)
-    g2.drawLine(100,100,400,400)
-
+    def g2 = initializeGraphics(imgPath, mazeImage)
+    def testPoints = [
+      [ a: [x: 100, y:100], b: [ x: 300, y: 300] ],
+      [ a: [x: 300, y:300], b: [ x: 50, y: 300] ]
+    ]
+    drawListOfPoints(g2, testPoints)
     def baos = new ByteArrayOutputStream()
     ImageIO.write( mazeImage, "png", baos)
     baos.flush()
@@ -35,6 +31,23 @@ class DrawPathService{
     baos.close()
 
     imageInByte
+  }
+
+
+  def initializeGraphics(def imgPath, def mazeImage){
+    def g2 = mazeImage.createGraphics()
+    def img1 = ImageIO.read(new File(imgPath))
+    g2.setBackground(Color.WHITE)
+    g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGTH)
+    g2.drawImage(img1, 0, 0, IMG_WIDTH, IMG_HEIGTH, null)
+    g2
+  }
+
+  def drawListOfPoints(def g2, def pointsList){
+    g2.setColor(Color.RED)
+    pointsList.each{ points ->
+      g2.drawLine(points.a.x, points.a.y, points.b.x, points.b.y)
+    }
   }
 
 }
